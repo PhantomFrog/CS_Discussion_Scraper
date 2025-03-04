738531b8ce255997fe47c5eb69514e4e4823d548
Rafael Gomes <rafael.matos.gomes@icloud.com>
2025-03-04 17:19:46 +0100

    shit works?

--- main.py
+++ main.py
@@ -1,10 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import itertools
+import threading
+import time
+import sys
 from autoscraper import AutoScraper
 
-url = 'https://steamcommunity.com/groups/csgo-ts'
+# URL of the Steam group
+url = 'https://steamcommunity.com/groups/csgo-ts/discussions'
 
-# We can add one or multiple candidates here.
-# You can also put urls here to retrieve urls.
-wanted_list = ["Knife"]
+# Loading animation
+done = False
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rLoading ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rDone!     \n')
+
+t = threading.Thread(target=animate)
+t.start()
+
+# Send a GET request to the URL
+response = requests.get(url)
+response.raise_for_status()  # Check if the request was successful
+
+# Parse the HTML content
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find all discussion topics
+topics = soup.find_all('div', class_='forum_topic')
+
+# Initialize AutoScraper
 scraper = AutoScraper()
-result = scraper.build(url, wanted_list)
-print(result)
+
+# Define the example of what you want to extract
+example = ["Bayonet"]
+
+# Build the scraper
+scraper.build(url, example)
+
+# Get the results
+results = scraper.get_result_similar(url, unique=True)
+
+# Filter topics containing the word "Bayonet" in the post content
+filtered_topics = []
+for topic in topics:
+    post_url = topic.find('a', class_='forum_topic_overlay')['href']
+    post_response = requests.get(post_url)
+    post_soup = BeautifulSoup(post_response.text, 'html.parser')
+    post_content = post_soup.find('div', class_='forum_op').text
+    if 'Bayonet' in post_content:
+        filtered_topics.append(post_content)
+
+# Stop the loading animation
+done = True
+t.join()
+
+# Print the filtered topics
+for topic in filtered_topics:
+    print(topic)
