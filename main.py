9743349dbff5435ad34db6486a54f06604db0874
Rafael Gomes <rafael.matos.gomes@icloud.com>
2025-03-04 18:51:46 +0100

    it works

--- main.py
+++ main.py
@@ -10,19 +10,22 @@ from autoscraper import AutoScraper
 # URL of the Steam group
 base_url = 'https://steamcommunity.com/groups/csgo-ts/discussions'
 
-# Keywords to search for
-keywords = ["Knife", "Bayonet"]
+# General keywords to search for
+general_keywords = ["AWP", "Neo-Noir"]
 
 # Number of pages to scrape
-num_pages = 3
+num_pages = 5
 
 # Loading animation
 done = False
+current_page = 1
+
 def animate():
+    global current_page
     for c in itertools.cycle(['|', '/', '-', '\\']):
         if done:
             break
-        sys.stdout.write('\rLoading ' + c)
+        sys.stdout.write(f'\rLoading page {current_page} ' + c)
         sys.stdout.flush()
         time.sleep(0.1)
     sys.stdout.write('\rDone!     \n')
@@ -33,15 +36,16 @@ t.start()
 # Initialize AutoScraper
 scraper = AutoScraper()
 
-# Define the example of what you want to extract
-example = ["Knife"]
+# Define the wanted list based on general keywords
+wanted_list = general_keywords
 
 # Build the scraper
-scraper.build(base_url, example)
+scraper.build(base_url, wanted_list=wanted_list)
 
 filtered_topics = []
 
 for page in range(1, num_pages + 1):
+    current_page = page
     url = f"{base_url}?p={page}"
     # Send a GET request to the URL
     response = requests.get(url)
@@ -56,14 +60,15 @@ for page in range(1, num_pages + 1):
     # Get the results
     results = scraper.get_result_similar(url, unique=True)
 
-    # Filter topics containing any of the keywords in the post content
+    # Filter topics containing any of the general keywords in the post content
     for topic in topics:
         post_url = topic.find('a', class_='forum_topic_overlay')['href']
         post_response = requests.get(post_url)
         post_soup = BeautifulSoup(post_response.text, 'html.parser')
-        post_content = post_soup.find('div', class_='forum_op').text
-        if any(keyword in post_content for keyword in keywords):
-            filtered_topics.append((post_url, post_content))
+        post_content = post_soup.find('div', class_='forum_op').text.lower()
+        found_keywords = [keyword for keyword in general_keywords if keyword.lower() in post_content]
+        if found_keywords:
+            filtered_topics.append((post_url, ', '.join(found_keywords)))
 
 # Stop the loading animation
 done = True
@@ -72,10 +77,9 @@ t.join()
 # Export the filtered topics to a CSV file
 with open('filtered_topics.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['URL', 'Content'])
+    writer.writerow(['URL', 'Keywords Found'])
     for topic in filtered_topics:
         writer.writerow(topic)
 
-# Print the filtered topics
-for topic in filtered_topics:
-    print(topic)
+
+
